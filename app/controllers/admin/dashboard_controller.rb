c11979096c10886d44abb2ac30efe5656c245c85
Scott Morton <smorton@Scotts-MacBook-Pro.local>
2020-11-11 20:27:33 -0700

    dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["admin_username"],password: ENV["admin_password"],except: [:show]  
+  http_basic_authenticate_with name: ENV["admin_username"],password: ENV["admin_password"],except: [:show] 
+  
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
